81ea330bba3b1d821acc0d940259221a3f90cf59
Mevinu Methdam <mevinumethdam@yahoo.com>
2026-09-09 19:50:33 +0530

    Notification model with id, title, message, and isRead fields.

---
+++ backend/models/Notification.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Notification = sequelize.define('Notification', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    message: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    isRead: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    }
+}, {
+    tableName: 'notifications',
+    timestamps: true
+});
+
+module.exports = Notification;
