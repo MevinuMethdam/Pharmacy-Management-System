8ed1ad699c63a1987a6026b7efa81800a31fcd8c
Mevinu Methdam <mevinumethdam@yahoo.com>
2026-09-14 00:46:06 +0530

    Update auth routes to include cashier and supplier login and setup routes, refactor login controller and add new routes for cashier and supplier login.

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { login, setupAdmin } = require('../controllers/authController');
+
+const { login, setupAdmin, cashierLogin, supplierLogin, setupCashier } = require('../controllers/authController');
 
 router.post('/login', login);
 
 router.post('/setup-admin', setupAdmin);
+router.post('/cashier-login', cashierLogin);
+
+router.post('/supplier-login', supplierLogin);
+router.post('/setup-cashier', setupCashier);
 
 module.exports = router;
